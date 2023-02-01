c2d99b70b777b1f592ca807700cb8ab95e8c0b6d
tr1ma1d <la3xmus@gmail.com>
2023-02-01 21:30:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Solution
+{
+    public static class Program
+    {
+        public static double basicOp(char op, double val1, double val2)
+        {
+            switch (op)
+            {
+                case '+': return val1 + val2;
+                case '-': return val1 - val2;
+                case '*': return val1 * val2;
+                case '/': return val1 / val2;
+                default:
+                    throw new System.ArgumentException("Unknown operation!", op.ToString());
+            }
+        }
+    }
+}
